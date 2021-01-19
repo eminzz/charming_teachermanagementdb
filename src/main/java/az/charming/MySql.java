b58335f45e62b10494ff74c10a245c087e6e5281
eminzz <e.zeynalov21@gmail.com>
2021-01-19 17:45:18 +0400

    dd

--- src/main/java/az/charming/MySql.java
+++ src/main/java/az/charming/MySql.java
@@ -164,7 +164,9 @@ localinda movcud olan branchlari gosterir.Branchlar arasinda kecid edende kodlar
 olsa sagda maven vurub dongu -kruq isaresine vurursan gelir),git checkout master yazanda kecir mastere, "idea faylini
 push edirik cunki bezen problemler aca bilir, meselen muellimde oldu brancha kecende dedi idea da workspace.xml var
 onu push et". usercrud branchinda System.out.println("usercrud"); app-main de bele kod yazdig masterden ferqlendirmek
-ucun ve kecid edende goreceyik ki masterde bu kod yoxdur.
+ucun ve kecid edende goreceyik ki masterde bu kod yoxdur, tebii ki etdiyin elaveleri gerek git add, commit falan edesen.
+
+ */
  */
  */
  */
